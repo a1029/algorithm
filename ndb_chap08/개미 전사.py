38575cb85344f5fb94b2210dc96a4e20c4c11cd0
a1029 <rttr1023@gmail.com>
2021-02-14 21:07:25 +0900

    21-02-14

---
+++ ndb_chap08/개미 전사.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+
+    # 자가풀이여부: X
+    def solution(self, arr: List[int]):
+        d = [0] * len(arr)
+        d[0] = arr[0]
+        d[1] = max(arr[0], arr[1])
+        for i in range(2, len(arr)):
+            d[i] = max(d[i - 1], d[i - 2] + arr[i])
+
+        print(max(d))
+
+
+p = Solution()
+p.solution([1, 3, 1, 5])
---
+++ ndb_chap08/효율적인 화폐 구성.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+
+    # 자가풀이여부: X
+    def solution(self, arr: List[int], m):
+
+        dp = [10000] * (m + 1)
+        dp[0] = 0
+
+        for n in arr:
+            for i in range(n, m + 1):
+                if dp[i - n] != 10000:
+                    dp[i] = min(dp[i], dp[i - n] + 1)
+
+        if dp[m] == 10000:
+            print(-1)
+        else:
+            print(dp[m])
+
+
+p = Solution()
+p.solution([2, 3], 15)
+p.solution([3, 5, 7], 4)
