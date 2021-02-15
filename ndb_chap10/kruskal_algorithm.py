7c4b756a33559d643671e53b2e6a5c681fac3ea2
a1029 <aab1023@naver.com>
2021-02-15 17:02:10 +0900

    21-02-15

---
+++ ndb_chap10/kruskal_algorithm.py
@@ -0,0 +1,52 @@
+from typing import List
+
+
+# 특정 원소가 속한 집합을 찾기
+# 경로 압축을 통해 호출할때마다 루트 노드 갱신이 가능하면 갱신
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+def kruskal_algorithm(v, e, edges: List[List[int]]):
+    parent = [0] * (v + 1)
+    result = 0
+
+    # 부모 테이블 상에서 부모를 자기 자신으로 초기화
+    for i in range(1, v + 1):
+        parent[i] = i
+
+    # 간선 비용을 기준으로 정렬
+    edges.sort(key=lambda x: x[2])
+
+    for u, v, w in edges:
+        # 두 노드의 루트 노드가 서로 같지 않다면(같은 집합에 포함된 것이 아니라면)
+        # 사이클이 아닌 것이며 집합에 포함
+        if find_parent(parent, u) != find_parent(parent, v):
+            union_parent(parent, u, v)
+            result += w
+
+    print(result)
+
+
+kruskal_algorithm(7, 9, [[1, 2, 29],
+                         [1, 5, 75],
+                         [2, 3, 35],
+                         [2, 6, 34],
+                         [3, 4, 7],
+                         [4, 6, 23],
+                         [4, 7, 13],
+                         [5, 6, 53],
+                         [6, 7, 25]])
---
+++ ndb_chap10/topology_sort.py
@@ -0,0 +1,44 @@
+import collections
+from typing import List
+
+
+def topology_sort(v, e, array: List[List[int]]):
+
+    # 모든 노드의 진입차수를 0으로 초기화
+    indegree = [0] * (v + 1)
+    graph = [[] for _ in range(v + 1)]
+    for a, b in array:
+        graph[a].append(b)
+        indegree[b] += 1
+
+    result = []
+    q = collections.deque()
+
+    for i in range(1, v + 1):
+        # 처음 시작노드는 진입차수가 0이므로 삽입
+        if indegree[i] == 0:
+            q.append(i)
+
+    while q:
+        # 진입차수가 0인 노드를 큐에서 추출
+        node = q.popleft()
+        result.append(node)
+        # 해당 노드와 연결된 인접노드들에 대하여
+        for i in graph[node]:
+            # 인접노드 연결 끊기(진입차수 -1)
+            indegree[i] -= 1
+            # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
+            if indegree[i] == 0:
+                q.append(i)
+
+    print(result)
+
+
+topology_sort(7, 8, [[1, 2],
+                     [1, 5],
+                     [2, 3],
+                     [2, 6],
+                     [3, 4],
+                     [4, 7],
+                     [5, 6],
+                     [6, 4]])
