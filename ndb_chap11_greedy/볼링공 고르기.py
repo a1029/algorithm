f33e59cbf9095a90a9aa0ccabb7e4668113b7f6f
a1029 <aab1023@naver.com>
2021-02-22 00:46:59 +0900

    21-02-19

---
+++ ndb_chap11_greedy/볼링공 고르기.py
@@ -0,0 +1,38 @@
+from typing import List
+import itertools
+import collections
+
+
+# 자가풀이여부: X
+def my_answer(data: List[int]):
+    counter = collections.Counter(data)
+    counter = sorted(counter.items())
+    n = len(data)
+    result = 0
+    for k, v in counter:
+        result += v * (n - v)
+        n -= v
+    print(result)
+
+
+def solution(data):
+    n = len(data)
+    m = max(data)
+    array = [0] * 11
+    for x in data:
+        array[x] += 1
+    result = 0
+    for i in range(1, m + 1):
+        n -= array[i]
+        result += array[i] * n
+
+    print(result)
+
+
+my_answer([1, 3, 2, 3, 2]) # 8
+my_answer([1, 5, 4, 3, 2, 4, 5, 2]) # 25
+my_answer([1, 3, 2, 5, 6, 4, 3, 2, 2, 1]) # 40
+solution([1, 3, 2, 3, 2])
+solution([1, 5, 4, 3, 2, 4, 5, 2])
+solution([1, 3, 2, 5, 6, 4, 3, 2, 2, 1])
+
