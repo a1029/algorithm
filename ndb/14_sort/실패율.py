c8d59620efb4504fa03ad5870fb24f2cf00cb0cd
a1029 <aab1023@naver.com>
2021-02-28 19:13:19 +0900

    21-02-28

---
+++ ndb/14_sort/실패율.py
@@ -0,0 +1,44 @@
+import collections
+
+# fail
+def my_answer(n, stages):
+    counter = [0] * (n + 2)
+    for stage in stages:
+        counter[stage] += 1
+    result = []
+    length = len(stages)
+    for i in range(1, len(counter) - 1):
+        if length == 0:
+            result.append((i, 0))
+        else:
+            result.append((i, counter[i] / length))
+        length -= counter[i]
+    result.sort(key=lambda x: -x[1])
+
+    return [x[0] for x in result]
+
+
+def solution(n, stages):
+    answer = []
+    length = len(stages)
+    for i in range(1, n + 1):
+        count = stages.count(i)
+
+        if length == 0:
+            fail = 0
+        else:
+            fail = count / length
+
+        answer.append((i, fail))
+        length -= count
+
+    answer = sorted(answer, key=lambda x: x[1], reverse=True)
+
+    return [x[0] for x in answer]
+
+
+
+print(my_answer(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+print(my_answer(4, [4, 4, 4, 4, 4]))
+print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+print(solution(4, [4, 4, 4, 4, 4]))
