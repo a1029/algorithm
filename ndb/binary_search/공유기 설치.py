04527b0be8e09aa8385256c681b1b84598f57ded
a1029 <rttr1023@gmail.com>
2021-03-01 20:31:44 +0900

    21-03-01

---
+++ ndb/binary_search/공유기 설치.py
@@ -0,0 +1,28 @@
+
+
+# fail
+def solution(n, c, data):
+    data.sort()
+
+    start = 1
+    end = data[-1] - data[0]
+    result = 0
+
+    while start <= end:
+        mid = (start + end) // 2
+        value = data[0]
+        count = 1
+        for i in range(1, n):
+            if data[i] >= value + mid:
+                value = data[i]
+                count += 1
+        if count >= c:
+            start = mid + 1
+            result = mid
+        else:
+            end = mid - 1
+
+    print(result)
+
+
+solution(5, 3, [1, 2, 8, 4, 9])
