e4c58613db3e8cacf87ebb75358e1c26584c3f2b
a1029 <rttr1023@gmail.com>
2021-03-14 22:50:15 +0900

    21-03-14

--- baekjoon/prefix_sum/10819. 차이를 최대로.py
+++ baekjoon/prefix_sum/10819. 차이를 최대로.py
@@ -12,3 +12,29 @@ for case in itertools.permutations(data):
     result = max(result, tmp)
 
 print(result)
+
+array = [0]*n
+visited = [False]*n
+result = 0
+def dfs(index):
+
+    global result
+
+    if index == n:
+        tmp = 0
+        for i in range(n-1):
+            tmp += abs(array[i]-array[i+1])
+        result = max(result, tmp)
+        return
+
+    else:
+        for i in range(n):
+            if visited[i]:
+                continue
+            visited[i] = True
+            array[index] = data[i]
+            dfs(index+1)
+            visited[i] = False
+
+dfs(0)
+print(result)
