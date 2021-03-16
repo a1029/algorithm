c8f3e47e7fd901af751aea160e33ebfc21e879b1
a1029 <rttr1023@gmail.com>
2021-03-16 23:56:33 +0900

    21-03-16

---
+++ baekjoon/shortest_path/11404.플로이드.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+n = int(input())
+m = int(input())
+graph = [[1e9]*(n+1) for _ in range(n+1)]
+for _ in range(m):
+    a,b,c = map(int, input().split())
+    graph[a][b] = c
+
+for i in range(n+1):
+    graph[i][i] = 0
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print(graph[i][j], end=' ')
+    print("")
