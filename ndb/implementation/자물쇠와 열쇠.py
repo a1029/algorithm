5fcffcdbce6d2cbc567ad82f255efe38661dde08
a1029 <rttr1023@gmail.com>
2021-03-29 03:27:36 +0900

    21-03-28

--- ndb/implementation/자물쇠와 열쇠.py
+++ ndb/implementation/자물쇠와 열쇠.py
@@ -1,47 +1,39 @@
-from typing import List
+def rotate(key):
+    n = len(key)
+    new_key = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            new_key[j][n-1-i] = key[i][j]
+    return new_key
 
-def rotate(key: List[List[int]]):
-    col = len(key)
-    row = len(key[0])
-    result = [[0] * col for _ in range(row)]
-    for i in range(col):
-        for j in range(row):
-            result[j][col-1-i] = key[i][j]
-    return result
-
-def check(new_lock: List[List[int]]):
-    lock_length = len(new_lock) // 3
-    for i in range(lock_length, lock_length*2):
-        for j in range(lock_length, lock_length*2):
-            if new_lock[i][j] != 1:
+def check(new_lock):
+    a = len(new_lock)//3
+    for i in range(a, a*2):
+        for j in range(a, a*2):
+            if new_lock[i][j]!=1:
                 return False
     return True
 
 def solution(key, lock):
-    n = len(lock)
     m = len(key)
-    # 자물쇠 크기 3배 확장
-    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
-    # 기존 자물쇠를 새로운 자물쇠의 중앙으로 이동
+    n = len(lock)
+    new_lock = [[0]*(n*3) for _ in range(n*3)]
     for i in range(n):
         for j in range(n):
-            new_lock[i + n][j + n] = lock[i][j]
+            new_lock[i+n][j+n] = lock[i][j]
 
-    # 4가지 방향에 대해
-    for rotation in range(4):
+    for _ in range(4):
         key = rotate(key)
-        for x in range(n * 2):
-            for y in range(n * 2):
-                # 자물쇠에 열쇠를 끼워 넣기
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] += key[i][j]
+        for a in range(n*2):
+            for b in range(n*2):
+                for c in range(m):
+                    for d in range(m):
+                        new_lock[a+c][b+d] += key[c][d]
                 if check(new_lock):
                     return True
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] -= key[i][j]
+                for c in range(m):
+                    for d in range(m):
+                        new_lock[a+c][b+d] -= key[c][d]
     return False
-
 
 print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
