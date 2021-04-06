6decbb1f3879e16f6226491d6b209c60ccd8122f
a1029 <rttr1023@gmail.com>
2021-04-06 21:43:13 +0900

    21-04-06

--- ndb/samsung_2020/청소년 상어.py
+++ ndb/samsung_2020/청소년 상어.py
@@ -1,6 +1,5 @@
 import copy
 
-
 arr = [[None]*4 for _ in range(4)]
 for i in range(4):
     data = list(map(int, input().split()))
@@ -13,6 +12,7 @@ result = 0
 def turn_left(direction):
     return (direction+1)%8
 
+
 def find_fish(arr, index):
     for i in range(4):
         for j in range(4):
@@ -20,45 +20,45 @@ def find_fish(arr, index):
                 return (i,j)
     return None
 
-def move_all_fished(arr,now_x,now_y):
-    for i in range(1,17):
-        pos = find_fish(arr, i)
-        if pos!=None:
-            x,y=pos[0],pos[1]
+
+def move_all_fish(arr, sx, sy):
+    for index in range(1,17):
+        pos = find_fish(arr, index)
+        if pos is not None:
+            x,y = pos[0], pos[1]
             direction = arr[x][y][1]
-            for j in range(8):
+            for _ in range(8):
                 nx = x + dx[direction]
                 ny = y + dy[direction]
-                if 0<=nx<4 and 0<=ny<4:
-                    if not (nx==now_x and ny==now_y):
-                        arr[x][y][1] = direction
-                        arr[x][y],arr[nx][ny] = arr[nx][ny],arr[x][y]
-                        break
+                if 0<=nx<4 and 0<=ny<4 and not (nx==sx and ny==sy):
+                    arr[x][y][1] = direction
+                    arr[x][y],arr[nx][ny] = arr[nx][ny], arr[x][y]
+                    break
                 direction = turn_left(direction)
 
-def get_possible_pos(arr,now_x,now_y):
-    positions = []
-    direction = arr[now_x][now_y][1]
+def possible_eat_to_fish(arr, sx, sy):
+    fishes = []
+    direction = arr[sx][sy][1]
     for i in range(4):
-        now_x += dx[direction]
-        now_y += dy[direction]
-        if 0<=now_x<4 and 0<=now_y<4:
-            if arr[now_x][now_y][0]!=-1:
-                positions.append((now_x,now_y))
-    return positions
+        sx += dx[direction]
+        sy += dy[direction]
+        if 0<=sx<4 and 0<=sy<4 and arr[sx][sy][0]!=-1:
+            fishes.append((sx,sy))
+    return fishes
 
-def dfs(data,now_x,now_y,total):
+def dfs(arr, sx, sy, total):
     global result
-    arr = copy.deepcopy(data)
-    total += arr[now_x][now_y][0]
-    arr[now_x][now_y][0] = -1
-    move_all_fished(arr,now_x,now_y)
-    positions = get_possible_pos(arr,now_x,now_y)
-    if len(positions)==0:
+    arr = copy.deepcopy(arr)
+    total += arr[sx][sy][0]
+    arr[sx][sy][0] = -1
+    move_all_fish(arr, sx, sy)
+    fishes = possible_eat_to_fish(arr, sx, sy)
+    if not fishes:
         result = max(result, total)
         return
-    for next_x,next_y in positions:
-        dfs(arr,next_x,next_y,total)
+    for x,y in fishes:
+        dfs(arr,x,y,total)
 
 dfs(arr,0,0,0)
 print(result)
+
