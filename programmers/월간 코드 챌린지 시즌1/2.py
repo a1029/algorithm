c300a38f733ff20e5104116232aa8ff8a51844eb
a1029 <rttr1023@gmail.com>
2021-04-16 01:04:57 +0900

    21-04-15

--- programmers/월간 코드 챌린지 시즌1/2.py
+++ programmers/월간 코드 챌린지 시즌1/2.py
@@ -6,35 +6,35 @@ def check(a):
 
 def solution(a, edges):
     tree = [[] for _ in range(len(a))]
+    indegree = [0]*(len(a))
     for i,j in edges:
         tree[i].append(j)
         tree[j].append(i)
-    roots = []
-    for i,j in enumerate(tree):
-        if len(j)==1:
-            roots.append(i)
+        indegree[i] += 1
+        indegree[j] += 1
 
-    for r in roots:
-        q = [r]
-        visit = [r]
-        res = 0
-        while q:
-            now = q.pop(0)
-            for nxt in tree[now]:
-                if nxt not in visit:
-                    if a[now]==0:
-                        t = a[nxt]
-                        a[now] += t
-                        a[nxt] -= t
-                        res += abs(t)
-                    else:
-                        t = a[now]
-                        a[now] -= t
-                        a[nxt] += t
-                        res += abs(t)
-                    q.append(nxt)
-                    visit.append(nxt)
-        print(res)
+    leaf = []
+    for i in range(len(indegree)):
+        if indegree[i]==1:
+            leaf.append(i)
+    visit = []
+    res = 0
+    while leaf:
+        now = leaf.pop(0)
+        visit.append(now)
+        for nxt in tree[now]:
+            if nxt not in visit:
+                t = a[now]
+                a[now] -= t
+                a[nxt] += t
+                res += abs(t)
+                indegree[nxt] -= 1
+                if indegree[nxt]==1:
+                    leaf.append(nxt)
+    if check(a):
+        return res
+    else:
+        return -1
 
 print(solution([-5,0,2,1,2],[[0,1],[3,4],[2,3],[0,3]]))
-#print(solution([0,1,0],[[0,1],[1,2]]))
+print(solution([0,1,0],[[0,1],[1,2]]))
