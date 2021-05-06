fa9626d00c1200f49185c6a7ef21f9f9433b3539
a1029 <rttr1023@gmail.com>
2021-05-06 21:53:27 +0900

    21-05-06

--- programmers/카카오_2019_겨울인턴십/크레인 인형뽑기 게임.py
+++ programmers/카카오_2019_겨울인턴십/크레인 인형뽑기 게임.py
@@ -2,16 +2,15 @@
 def solution(board, moves):
 
     answer = 0
-    n = len(board)
     basket = []
-    for m in moves:
-        for i in range(n):
-            if board[i][m-1]!=0:
-                basket.append(board[i][m-1])
-                board[i][m-1] = 0
+    for j in moves:
+        for i in range(len(board)):
+            if board[i][j-1]!=0:
+                basket.append(board[i][j-1])
+                board[i][j-1] = 0
                 break
         if len(basket)>=2 and basket[-1]==basket[-2]:
+            answer+=2
             basket.pop()
             basket.pop()
-            answer += 2
     return answer
