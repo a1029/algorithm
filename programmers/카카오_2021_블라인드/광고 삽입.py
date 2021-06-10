cd855aaab6a35034ab33845f923f993e6573dc36
rttr1023 <rttr1023@gmail.com>
2021-06-11 00:53:36 +0900

    21-06-11

--- programmers/카카오_2021_블라인드/광고 삽입.py
+++ programmers/카카오_2021_블라인드/광고 삽입.py
@@ -1,44 +1,41 @@
-import collections
 
 
-def get_second(data):
-    times = data.split(':')
-    return int(times[0])*3600 + int(times[1])*60 + int(times[2])
+def to_sec(x):
+    t = x.split(':')
+    return int(t[0])*3600 + int(t[1])*60 + int(t[2])
 
+def to_str(x):
+    m,s = divmod(x, 60)
+    h,m = divmod(m, 60)
+    return str(h).zfill(2)+':'+str(m).zfill(2)+':'+str(s).zfill(2)
 
 def solution(play_time, adv_time, logs):
 
-    play_time_sec = get_second(play_time)
-    adv_time_sec = get_second(adv_time)
-    logs_start_sec = []
-    logs_end_sec = []
+    pt = to_sec(play_time)
+    at = to_sec(adv_time)
+    tt = [0]*360001
     for log in logs:
-        logs_start_sec.append(get_second(log.split('-')[0]))
-        logs_end_sec.append(get_second(log.split('-')[1]))
+        start, end = map(to_sec, log.split('-'))
+        tt[start] += 1
+        tt[end] -= 1
 
-    total_time = collections.defaultdict(int)
-    for i in range(len(logs)):
-        total_time[logs_start_sec[i]] = total_time[logs_start_sec[i]] + 1
-        total_time[logs_end_sec[i]] = total_time[logs_end_sec[i]] - 1
+    for i in range(1, pt):
+        tt[i] += tt[i-1]
+    for i in range(1, pt):
+        tt[i] += tt[i-1]
 
-    for i in range(1, play_time_sec):
-        total_time[i] = total_time[i] + total_time[i-1]
-
-    for i in range(1, play_time_sec):
-        total_time[i] = total_time[i] + total_time[i-1]
-
-    max_time = 0
-    for i in range(adv_time_sec-1, play_time_sec):
-        if i:
-            max_time = max(max_time, total_time[i] - total_time[i-at])
+    mt = 0
+    answer = 0
+    for i in range(at-1, pt):
+        if i>=at:
+            if mt < tt[i]-tt[i-at]:
+                mt = tt[i]-tt[i-at]
+                answer = i-at+1
         else:
-            max_time = max(max_time, total_time[i])
-
-    h,mod = divmod(max_time, 60)
-    m,s = divmod(mod, 60)
-    answer = str(h)+':'+str(m)+':'+str(s)
-    print(answer)
-    return answer
+            if mt < tt[i]:
+                mt = tt[i]
+                answer = i-at+1
+    return to_str(answer)
 
 
 solution("02:03:55","00:14:15",["01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"])
