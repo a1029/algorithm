7197c8cc48553009cd24f6a662bc2cd888b52329
J065_김한빈 <rttr1023@gmail.com>
2021-09-11 20:22:31 +0900

    구현 1개

---
+++ java/src/implementation/숫자문자열과영단어.java
@@ -0,0 +1,25 @@
+package implementation;
+
+public class 숫자문자열과영단어 {
+
+    public static void main(String[] args) {
+
+        System.out.println(solution("23foursix5sixsix7"));
+    }
+
+    public static int solution(String s) {
+
+        s = s
+                .replaceAll("one", "1")
+                .replaceAll("two", "2")
+                .replaceAll("three", "3")
+                .replaceAll("four", "4")
+                .replaceAll("five", "5")
+                .replaceAll("six", "6")
+                .replaceAll("seven", "7")
+                .replaceAll("eight", "8")
+                .replaceAll("nine", "9");
+        return Integer.parseInt(s);
+    }
+
+}
