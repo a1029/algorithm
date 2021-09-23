814420b9c9a517260381af9eb743c48f9dfa4e62
J065_김한빈 <rttr1023@gmail.com>
2021-09-23 21:26:17 +0900

    String 1

--- java/src/String/Strfry.java
+++ java/src/String/Strfry.java
@@ -1,48 +1,42 @@
-//package String;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.StringTokenizer;
-//
-//public class Strfry {
-//
-//    static ArrayList<String[]> result = new ArrayList<>();
-//    static void permutation(String[] arr, int depth, int n, int r) {
-//        if (depth == r) {
-//            result.add(arr);
-//            return;
-//        }
-//
-//        for (int i=depth; i<n; i++) {
-//            swap(arr, depth, i);
-//            permutation(arr, depth + 1, n, r);
-//            swap(arr, depth, i);
-//        }
-//
-//    }
-//
-//    static void swap(String[] arr, int depth, int i) {
-//        String temp = arr[depth];
-//        arr[depth] = arr[i];
-//        arr[i] = temp;
-//    }
-//
-//    public static void main(String[] args) throws IOException {
-//
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//
-//        int tc = Integer.parseInt(br.readLine());
-//        while(tc-->0){
-//            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-//            String[] a = st.nextToken().split("");
-//            String b = st.nextToken();
-//            permutation(a, a.length, a.length, a.length);
-//            for(int i=0; i<result.size(); i++){
-//                System.out.println(Arrays.toString(result.get(i)));
-//            }
-//        }
-//    }
-//}
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Strfry {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine());
+        while(tc-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            String[] a = st.nextToken().split("");
+            String[] b = st.nextToken().split("");
+            int[] count = new int[26];
+            Arrays.fill(count, 0);
+            for(String s : a){
+                count[s.charAt(0)-'a'] += 1;
+            }
+            for(String s : b){
+                count[s.charAt(0)-'a'] -= 1;
+            }
+
+            boolean flag = false;
+            for(int c : count){
+                if(c!=0){
+                    System.out.println("Impossible");
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                System.out.println("Possible");
+            }
+        }
+    }
+}
