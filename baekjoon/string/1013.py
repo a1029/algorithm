47f4e8be6d347f25a8ac1963307684d22e220f48
J065_김한빈 <rttr1023@gmail.com>
2021-12-13 18:24:57 +0900

    백준 문자열

--- baekjoon/string/1013.py
+++ baekjoon/string/1013.py
@@ -1,26 +1,11 @@
-def solution(strs):
-    answer = 0
-    for str in strs:
-        stack = []
-        for i in range(len(str)-1, -1, -1):
-            stack.append(str[i])
-            if len(stack) > 1:
-                if str[i] == "A":
-                    if stack[-1] == "A" and "A" not in stack[:-1]:
-                        stack.clear()
-                else:
-                    if stack[-1] == "A":
-                        stack.clear()
-        if not stack:
-            answer += 1
-    return answer
-
-
-# (AAB~|BAB~A)~
-print(solution(["AABAAA", "BABABB", "BABBAAAB", "BABAAABAABBABBA"]))
-print(solution(["AA", "BAB", "BAAAA", "ABBABB", "AABBBBABBAAAA"]))
-print(solution(["AABAABAAB", "AABBBAABBB", "AABBBABBABABBBAAABBBABBBA"]))
-
-print(solution(["10010111"]))
-print(solution(["011000100110001"]))
-print(solution(["0110001011001"]))
+import re
+t = int(input())
+p = re.compile('(100+1+|01)+')
+for _ in range(t):
+    string = input()
+    result = p.fullmatch(string)
+    if result:
+        print("YES")
+    else:
+        print("NO")
+    
