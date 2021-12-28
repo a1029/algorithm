d79e19e64f733f91905043c7b637b2e66fcf2428
J065_김한빈 <rttr1023@gmail.com>
2021-12-28 20:21:33 +0900

    가장 큰 정사각형 찾기

---
+++ programmers/연습문제/동적프로그래밍/가장 큰 정사각형 찾기.py
@@ -0,0 +1,12 @@
+def solution(board):
+    answer = 0
+    if len(board)<2 and len(board[0]) < 2:
+        return board[0][0]
+    for i in range(1,len(board)):
+        for j in range(1,len(board[0])):
+            if board[i][j] != 0:
+                board[i][j] = min(board[i-1][j], board[i-1][j-1], board[i][j-1]) + 1
+                answer = max(answer, board[i][j])
+    return answer*answer
+
+print(solution())
