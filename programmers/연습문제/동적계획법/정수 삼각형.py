8d8c379dc5f1878e57443023ce28ce2ee2668915
J065_김한빈 <rttr1023@gmail.com>
2022-01-03 16:59:57 +0900

    정수 삼각형

---
+++ programmers/연습문제/동적계획법/정수 삼각형.py
@@ -0,0 +1,17 @@
+def solution(triangle):
+    answer = 0
+    n = len(triangle)
+    dp = [[0]*(i+1) for i in range(n)]
+    dp[0][0] = triangle[0][0]
+    for i in range(1, n):
+        for j in range(len(triangle[i])):
+            if j==0:
+                dp[i][j] = dp[i-1][j] + triangle[i][j]
+            elif j==len(triangle[i])-1:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+    
+    return max(dp[n-1])
+
+print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
