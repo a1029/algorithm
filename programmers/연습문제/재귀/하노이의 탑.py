32e04e50c2b9098c7723e171a0f8265dfec69000
J065_김한빈 <rttr1023@gmail.com>
2022-01-04 19:12:46 +0900

    하노이의 탑

---
+++ programmers/연습문제/재귀/하노이의 탑.py
@@ -0,0 +1,16 @@
+def hanoi(answer, n, start, mid, end):
+    if n == 1:
+        answer.append([start, end])
+        return
+    hanoi(answer, n-1, start, end, mid)
+    answer.append([start, end])
+    hanoi(answer, n-1, mid, start, end)
+    
+    
+
+def solution(n):
+    answer = []
+    hanoi(answer, n, 1, 2, 3)
+    return answer
+
+print(solution(20))
